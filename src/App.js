d1eb25766f9b930bcf783eead1c05034d8e23f3e
Insignificant_Guy <zakramisaway@gmail.com>
2021-06-14 02:37:36 +0530

    Used React Router in the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import HomePage from './pages/homepage/homepage.component.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './pages/homepage/homepage.style.scss';
+
+const HatsPage = (props) =>{
+  return (
+    <div>
+      <h1>This is a Hat Page</h1>
+    </div>
+  )
+};
 
 function App() {
   return( 
-    <div>
-      <HomePage/>
-    </div>
+    <BrowserRouter>
+        <Route exact path="/" component={ HomePage }/>
+        <Route exact path="/shop/hats" component={ HatsPage }/>
+    </BrowserRouter>
   )
 }
 
