d4e529870b31ec9478d1c0534ee62be1f8a7e14d
Insignificant_Guy <zakramisaway@gmail.com>
2021-06-17 01:55:25 +0530

    Added shop section in the website

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import HomePage from './pages/homepage/homepage.component.jsx';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './pages/homepage/homepage.style.scss';
-
-const HatsPage = (props) =>{
-  return (
-    <div>
-      <h1>This is a Hat Page</h1>
-    </div>
-  )
-};
+import ShopPage from './pages/shop/shop.component.jsx';
+import './App.css';
 
 function App() {
   return( 
     <BrowserRouter>
         <Route exact path="/" component={ HomePage }/>
-        <Route exact path="/shop/hats" component={ HatsPage }/>
+        <Route exact path="/shop" component={ ShopPage }/>
     </BrowserRouter>
   )
 }
