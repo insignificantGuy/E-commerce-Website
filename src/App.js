5e065cdf7e274db416ae06d20ca725a5a5eef511
Insignificant_Guy <zakramisaway@gmail.com>
2021-06-18 02:28:36 +0530

    Added Header to our shop page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import HomePage from './pages/homepage/homepage.component.jsx';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './pages/homepage/homepage.style.scss';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import './App.css';
 
 function App() {
   return( 
+    <div>
     <BrowserRouter>
-        <Route exact path="/" component={ HomePage }/>
-        <Route exact path="/shop" component={ ShopPage }/>
-    </BrowserRouter>
-  )
+      <Header/>
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
