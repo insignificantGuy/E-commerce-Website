bc85c8b55151c77576b74d1615291165d96bd7d6
insignificantGuy <zakramisaway@gmail.com>
2022-02-14 16:41:08 +0530

    Learned Higher Order Component

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,65 +1,57 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components';
 
-const ButtonStyles = css`
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
+const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  &:hover {
     background-color: black;
     color: white;
     border: none;
-
-    &:hover{
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-    }
-
+  }
 `;
 
-const InvertedButtonStyles = css`
-    background-color: black;
-    color: white;
-    border: 1px solid black;
-
-    &:hover{
-    background-color: white;
-    color:black;
-    border:1px solid black;
-    }
-`;
-
-const GoogleSignInStyles = css`
-    background-color: #4285f4;
-    color:white;
-    border:none;
-
-    &:hover{
+const googleSignInStyles = css`
+  background-color: #4285f4;
+  color: white;
+  &:hover {
     background-color: #357ae8;
-    border:none;
-    }
+    border: none;
+  }
 `;
 
 const getButtonStyles = props => {
-    console.log(props);
-    if (props.isGoogleSignIn){
-        return GoogleSignInStyles;
-    }
-    return props.inverted ? InvertedButtonStyles: ButtonStyles;
-}
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
 
 export const CustomButtonContainer = styled.button`
-    min-width: 165px;
-    width: auto;
-    height: 50px;
-    letter-spacing: 0.5px;
-    line-height: 50px;
-    padding: 0 35px 0 35px;
-    font-size: 15px;
-    text-transform: uppercase;
-    font-family: 'Open Sans Condensed';
-    font-weight: bolder;
-    cursor: pointer;
-    display:flex;
-    justify-content: center;
-
-    ${getButtonStyles}
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: 'Open Sans Condensed';
+  font-weight: bolder;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  ${getButtonStyles}
 `;
-
-
